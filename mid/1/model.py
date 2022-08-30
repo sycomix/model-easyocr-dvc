f754459530695367a792d4d6aed22459ada2cf84
Phelan164 <phelan.nguyen@instill.tech>
2022-08-30 15:29:14 +0700

    chore: update add free list to ocr image list

--- mid/1/model.py
+++ mid/1/model.py
@@ -1,17 +1,107 @@
 import numpy as np
 import json
 import math
-import os
+
+from PIL import Image
 import cv2
 import torch
 import torchvision.transforms as transforms
 
-from random import choice
 from typing import List
 
 from triton_python_backend_utils import Tensor, InferenceResponse, \
     get_input_tensor_by_name, InferenceRequest, get_input_config_by_name, \
     get_output_config_by_name, triton_string_to_numpy
+
+
+def four_point_transform(image, rect):
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([[0, 0],[maxWidth - 1, 0],[maxWidth - 1, maxHeight - 1],[0, maxHeight - 1]], dtype = "float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    return warped
+
+def calculate_ratio(width,height):
+    '''
+    Calculate aspect ratio for normal use case (w>h) and vertical text (h>w)
+    '''
+    ratio = width/height
+    if ratio<1.0:
+        ratio = 1./ratio
+    return ratio
+
+def compute_ratio_and_resize(img,width,height,model_height):
+    '''
+    Calculate ratio and resize correctly for both horizontal text
+    and vertical case
+    '''
+    ratio = width/height
+    if ratio<1.0:
+        ratio = calculate_ratio(width,height)
+        img = cv2.resize(img,(model_height,int(model_height*ratio)), interpolation=Image.ANTIALIAS)
+    else:
+        img = cv2.resize(img,(int(model_height*ratio),model_height),interpolation=Image.ANTIALIAS)
+    return img,ratio
+
+def get_image_list(horizontal_list, free_list, img, model_height = 64, sort_output = True):
+    image_list = []
+    maximum_y,maximum_x = img.shape
+
+    max_ratio_hori, max_ratio_free = 1,1
+    for box in free_list:
+        rect = np.array(box, dtype = "float32")
+        transformed_img = four_point_transform(img, rect)
+        ratio = calculate_ratio(transformed_img.shape[1],transformed_img.shape[0])
+        new_width = int(model_height*ratio)
+        if new_width == 0:
+            pass
+        else:
+            crop_img,ratio = compute_ratio_and_resize(transformed_img,transformed_img.shape[1],transformed_img.shape[0],model_height)
+            image_list.append( (box,crop_img) ) # box = [[x1,y1],[x2,y2],[x3,y3],[x4,y4]]
+            max_ratio_free = max(ratio, max_ratio_free)
+
+
+    max_ratio_free = math.ceil(max_ratio_free)
+
+    for box in horizontal_list:
+        x_min = max(0,box[0])
+        x_max = min(box[1],maximum_x)
+        y_min = max(0,box[2])
+        y_max = min(box[3],maximum_y)
+        crop_img = img[y_min : y_max, x_min:x_max]
+        width = x_max - x_min
+        height = y_max - y_min
+        ratio = calculate_ratio(width,height)
+        new_width = int(model_height*ratio)
+        if new_width == 0:
+            pass
+        else:
+            crop_img,ratio = compute_ratio_and_resize(crop_img,width,height,model_height)
+            image_list.append( ( [[x_min,y_min],[x_max,y_min],[x_max,y_max],[x_min,y_max]] ,crop_img) )
+            max_ratio_hori = max(ratio, max_ratio_hori)
+
+    max_ratio_hori = math.ceil(max_ratio_hori)
+    max_ratio = max(max_ratio_hori, max_ratio_free)
+    max_width = math.ceil(max_ratio)*model_height
+
+    if sort_output:
+        image_list = sorted(image_list, key=lambda item: item[0][0][1]) # sort by vertical position
+    return image_list, max_width
 
 
 def group_text_box(polys, slope_ths = 0.1, ycenter_ths = 0.5, height_ths = 0.5, width_ths = 1.0, add_margin = 0.05, sort_output = True):
@@ -460,11 +550,13 @@ class TritonPythonModel(object):
 
             img_idx = 0
             map_image_boxes = {}
+            box_shapes = []
             for image, mask, scale_img in zip(batch_in['image'], batch_in['mask'], batch_in['scale']): # img is shape (1,)
                 img_idx += 1
                 image_h, image_w = image.shape[:2]
                 org_img = cv2.resize(np.array(image), (int(image_w*scale_img[0]) ,int(image_h*scale_img[1])), interpolation = cv2.INTER_AREA)
-                map_image_boxes[img_idx] = {"image": org_img, "bbox": []}
+                img_cv_grey = cv2.cvtColor(org_img, cv2.COLOR_BGR2GRAY)
+
                 score_text = mask[:, :, 0]
                 score_link = mask[:, :, 1]
                 boxes, polys, _ = getDetBoxes(
@@ -476,34 +568,43 @@ class TritonPythonModel(object):
                     if polys[k] is None:
                         polys[k] = boxes[k]
                 text_box = [np.array(box).astype(np.int32).reshape((-1)) for box in polys]
-                horizontal_list, _ = group_text_box(text_box, slope_ths,
+                horizontal_list, free_list = group_text_box(text_box, slope_ths,
                                                 ycenter_ths, height_ths,
                                                 width_ths, add_margin,
                                                 (optimal_num_chars is None))
-                for bb in horizontal_list:
-                    x1, x2, y1, y2 = bb
+
+                image_list1, _ = get_image_list(horizontal_list, [], img_cv_grey, model_height = 64)
+                image_list2, _ = get_image_list([], free_list, img_cv_grey, model_height = 64)
+                image_list = image_list1 + image_list2
+
+                coord = [item[0] for item in image_list]
+                img_list = [item[1] for item in image_list]
+
+                map_image_boxes[img_idx] = {"image": [], "bbox": []}
+                for cor, roi_img in zip(coord, img_list):
+                    x1, y1, x2, y2 = int(cor[0][0]), int(cor[0][1]), int(cor[2][0]), int(cor[2][1])
                     if x1 < 0: x1 = 0
                     if x2 < 0: x2 = 0
                     if y1 < 0: y1 = 0
                     if y2 < 0: y2 = 0 
+                    box_shapes.append(roi_img.shape)
                     map_image_boxes[img_idx]["bbox"].append([x1, y1, x2-x1, y2-y1])
+                    map_image_boxes[img_idx]["image"].append(roi_img)
                     batch_out['bbox'].append([x1, y1, x2 - x1, y2 - y1])
                     batch_out['mbbox'].append(img_idx)
                     
-                if len(horizontal_list) == 0: # add dummy boxes for image which have no textline
+                if len(image_list) == 0: # add dummy boxes for image which have no textline
                     batch_out['mbbox'].append(img_idx)
                     batch_out['bbox'].append([-1, -1, -1, -1])
 
             if len(batch_out['bbox']) > 0:
-                max_w = max([math.ceil(bb[2]/bb[3])*64 for bb in batch_out['bbox']])
+                max_w = max([math.ceil(box_shape[1]/box_shape[0])*64 for box_shape in box_shapes])
                 normalizePAD = NormalizePAD((1, 64, max_w))
                 for _, image_box in map_image_boxes.items():
-                    org_img = image_box["image"]
-                    for bb in image_box["bbox"]:  # img is shape (1,)
-                        x, y, w, h = bb
-                        roi = org_img[y:y+h, x:x+w]
+                    for roi in image_box["image"]:  # img is shape (1,)
+                        h, w = np.array(roi).shape[:2]
                         roi = cv2.resize(np.array(roi), (int(w*64/h) ,64), interpolation = cv2.INTER_AREA)
-                        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY) / 255.0
+                        roi = roi / 255.0
                         roi = normalizePAD.normalize(roi)
                         roi = np.array(roi).astype("float32")
                         batch_out['textline'].append(roi)
