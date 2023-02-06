c89d1a0b283c77fc139e9da08664d89e64e45177
Phelan164 <phelan.nguyen@instill.tech>
2023-02-06 13:34:48 +0700

    chore: update import

--- pre/1/model.py
+++ pre/1/model.py
@@ -5,8 +5,8 @@ from typing import List
 from PIL import Image
 import cv2
 
-from triton_python_backend_utils import Tensor, InferenceResponse, \
-    get_input_tensor_by_name, InferenceRequest, get_output_config_by_name, triton_string_to_numpy
+from triton_python_backend_utils import get_output_config_by_name, triton_string_to_numpy, get_input_config_by_name
+from c_python_backend_utils import Tensor, InferenceResponse, InferenceRequest
 
 
 def normalizeMeanVariance(in_img, mean=(0.485, 0.456, 0.406), variance=(0.229, 0.224, 0.225)):
